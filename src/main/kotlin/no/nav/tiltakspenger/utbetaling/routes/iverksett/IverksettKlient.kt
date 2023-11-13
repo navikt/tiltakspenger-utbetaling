a3a31576e2f8f01793f555382d1165124f085366
Benedicte Myrvoll <benemyr17@gmail.com>
2023-11-13 15:27:53 +0100

    F Legg til håndtering av ulike responsmeldinger fra tiltakspenger-iverksett og bruk iverksettDto
    Co-authored-by: KarlEvaldNav <Karl.Evald.Wigestrand@nav.no>

--- src/main/kotlin/no/nav/tiltakspenger/utbetaling/routes/iverksett/IverksettKlient.kt
+++ src/main/kotlin/no/nav/tiltakspenger/utbetaling/routes/iverksett/IverksettKlient.kt
@@ -6,6 +6,7 @@ import io.ktor.client.request.bearerAuth
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import io.ktor.server.config.ApplicationConfig
 import no.nav.dagpenger.kontrakter.iverksett.IverksettDto
@@ -19,16 +20,46 @@ class IverksettKlient(
     private val iverksettEndpoint = config.property("endpoints.iverksett").getString()
     private val log = LoggerFactory.getLogger(this::class.java)
 
-    override suspend fun iverksett(utbetalingDTOUt: IverksettDto): String {
+    override suspend fun iverksett(iverksettDto: IverksettDto): String {
+        try {
+
         val token = iverksettCredentialsClient.getToken()
         val res = client.post("$iverksettEndpoint/api/iverksetting") {
             accept(ContentType.Application.Json)
             contentType(ContentType.Application.Json)
-            setBody(objectMapper.writeValueAsString(utbetalingDTOUt))
+            setBody(objectMapper.writeValueAsString(iverksettDto))
             bearerAuth(token)
         }
         log.info("status fra dp-iverkett er ${res.status}")
 
-        return "IKKE NOE HER ER FERDIG ENDA"
+        when (res.status) {
+            HttpStatusCode.Accepted -> {
+                log.info("Iverksetting er mottat for behandlingId ${iverksettDto.behandlingId}")
+                return "Iverksetting er mottat for behandlingId ${iverksettDto.behandlingId} ${res.status}"
+            }
+            HttpStatusCode.BadRequest -> {
+                log.info("Ugyldig format på iverksetting for behandlingId ${iverksettDto.behandlingId}")
+                return "Ugyldig format på iverksetting for behandlingId ${iverksettDto.behandlingId} ${res.status}"
+            }
+            HttpStatusCode.Forbidden -> {
+                log.info("Ikke autorisert til å starte iverksetting for behandlingId ${iverksettDto.behandlingId}")
+                return "Ikke autorisert til å starte iverksetting for behandlingId ${iverksettDto.behandlingId} ${res.status}"
+            }
+            HttpStatusCode.Conflict -> {
+                log.info("Iverksetting er i konflikt med tidligere iverksetting for behandlingId ${iverksettDto.behandlingId}")
+                return "Iverksetting er i konflikt med tidligere iverksetting for behandlingId ${iverksettDto.behandlingId} ${res.status}"
+            }
+
+            else -> {
+                log.error("Kallet til tiltakspenger-iverksett feilet ${res.status} ${res.status.description}")
+                throw RuntimeException("Feil i kallet til iverksett")
+            }
+        }
+    } catch (throwable: Throwable) {
+            log.warn("Uhåndtert feil mot tiltakspenger-iverksett. Mottat feilmelding ${throwable.message}")
+            return "Uhåndtert feil mot tiltakspenger-iverksett. Mottat feilmelding ${throwable.message}"
+        }
     }
 }
+
+
